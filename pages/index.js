409bf67166ca253b3b983b665816a975eca5738b
Wladimir Segura <69754237+wladimirsegura@users.noreply.github.com>
2023-03-11 10:42:47 +0000

    index.js updated

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
 function Home(){
-    return <h1>Teste de servidor</h1>
+    return <div>
+        <h1>Oi Filipe!!</h1>
+        <p>No final das contas fui pelo caminho mais facil de encontrar a reacao de uma pessoa kkkk<br></br>
+          Somente gostaria de deixar registrado meu muitisssimo agradecimento pelo seu<br></br>
+          (e do Guga, da Renata, do editor tb) trabalho que voces vem fazendo no curso !!<br></br>
+          Nao trabalho na area de TI, nem tinha ideia como usar o Git/gitHub, nem o Next.js e React,<br></br>
+          Fiz como voce falou , joguei a bola pelo muro e fui buscar... Para minha surpresa consegui chegar ate aqui!!
+        </p>
+        <strong>Wladimir Segura (wladi96)</strong>
+    </div>
 }
 
 export default Home;
