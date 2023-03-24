a065ae2aeccca2af9b76b95cab60b9001dffcf78
Wladimir Segura <69754237+wladimirsegura@users.noreply.github.com>
2023-03-24 13:48:10 +0000

    add .gitignore

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 function Home(){
     return <div>
         <h1>Oi Filipe!!</h1>
-        <p>No final das contas fui pelo caminho mais facil de encontrar a reacao de uma pessoa kkkk<br></br>
-          Somente gostaria de deixar registrado meu muitisssimo agradecimento pelo seu<br></br>
-          (e do Guga, da Renata, do editor tb) trabalho que voces vem fazendo no curso !!<br></br>
-          Nao trabalho na area de TI, nem tinha ideia como usar o Git/gitHub, nem o Next.js e React,<br></br>
+        <p>No final das contas fui pelo caminho mais facil de encontrar a reacao de uma pessoa kkkk<br/>
+          Somente gostaria de deixar registrado meu muitisssimo agradecimento pelo seu<br/>
+          (e do Guga, da Renata, do editor tb) trabalho que voces vem fazendo no curso !!<br/>
+          Nao trabalho na area de TI, nem tinha ideia como usar o Git/gitHub, nem o Next.js e React,<br/>
           Fiz como voce falou , joguei a bola pelo muro e fui buscar... Para minha surpresa consegui chegar ate aqui!!
         </p>
         <strong>Wladimir Segura (wladi96)</strong>
